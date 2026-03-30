9ab252b84dc61a5b90139323e08610f7b5247e05
Major1111111 <vojs1077@gmail.com>
2026-03-30 20:21:35 +0300

    Update java.js

--- java.js
+++ java.js
@@ -20,4 +20,17 @@ bot.on("message:web_app_data", async (ctx) => {
 });
 
 bot.start(); 
- 
+ const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(Server is running on port ${port});
+});
