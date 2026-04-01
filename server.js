63d58663533552ae1f147d94437e5bc799b4684d
Major1111111 <vojs1077@gmail.com>
2026-04-01 17:04:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,24 @@ const PORT = 3000;
 // Этой строкой мы говорим серверу показывать файлы из папки public
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Массив для хранения игроков
+let players = [];
+
+// Функция для инициализации игроков
+function initializePlayers(numPlayers) {
+    for (let i = 0; i < numPlayers; i++) {
+        players.push({ id: i, tons: 10 }); // Каждому игроку по 10 тонн
+    }
+}
+
+// Инициализируем 4 игроков (например)
+initializePlayers(4);
+
 // Если кто-то заходит на главную страницу, отдаем index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Сервер запущен! Адрес в браузере: http://localhost:${PORT}`);
+    console.log('Сервер запущен! Адрес в браузере: http://localhost:${PORT}');
 });
